dd5fc92c7877a1a555f018d85c5a18c49b5f01c3
abhishek7377Sonu <abhishek73770@gmail.com>
2025-05-25 12:29:14 +0530

    Update BootDataJpaProj5FinderMethodsApplication_StaticProjectionTests.java

--- src/test/java/com/nt/BootDataJpaProj5FinderMethodsApplication_StaticProjectionTests.java
+++ src/test/java/com/nt/BootDataJpaProj5FinderMethodsApplication_StaticProjectionTests.java
@@ -3,7 +3,8 @@ package com.nt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BootDataJpaProj5FinderMethodsApplication.class)
+
 class BootDataJpaProj5FinderMethodsApplication_StaticProjectionTests {
 
 	@Test
